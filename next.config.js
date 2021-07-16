3893b8c8b65e1a0a37aed95d53866807b0e1a0b8
Pedro de Paula <pmdpaula@yahoo.com.br>
2021-07-16 19:09:30 -0300

    chore: add packages next-compose-plugins and next-images
    Adição dos pacotes next-compose-plugins e next-images e recontrução do next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
 const redirects = require('./config/redirects');
 
-module.exports = {
+const withImagesConfig = {
+  assetPrefix: 'https://github.com',
+  dynamicAssetPrefix: true,
+  // eslint-disable-next-line no-unused-vars
+  webpack(config, options) {
+    return config;
+  },
+};
+
+module.exports = withPlugins([withImages, { withImagesConfig }], {
   trailingSlash: true,
   typescript: {
     // !! WARN !!
@@ -13,4 +25,18 @@ module.exports = {
   async redirects() {
     return redirects;
   },
-};
+});
+
+// module.exports = {
+//   trailingSlash: true,
+//   typescript: {
+//     // !! WARN !!
+//     // Dangerously allow production builds to successfully complete even if
+//     // your project has type errors.
+//     // !! WARN !!
+//     ignoreBuildErrors: true,
+//   },
+//   async redirects() {
+//     return redirects;
+//   },
+// };
